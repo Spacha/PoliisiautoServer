741ee07d84e5cb41609daded57aaf7c4cd118115
Spacha <miikasikala96@gmail.com>
2022-09-23 05:52:49 +0300

    Added some basics

---
+++ app/helpers.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
+
+/**
+ * Called by router when a type hinted resource is not found.
+ */
+function resourceNotFound(Request $request) {
+    return responseError("Resource not found", 404);
+}
+
+/**
+ * A success response. Always contains a message.
+ */
+function responseOk(string $message, array $extra = [], int $status = 200) : JsonResponse
+{
+    return response()->json([
+        "message" => $message
+    ] + $extra, $status);
+}
+
+/**
+ * An error response. Contains an error message, or an array of error messages.
+ */
+function responseError($errors, int $status = 400) : JsonResponse
+{
+    return response()->json([
+        "errors" => $errors
+    ], $status);
+}
+
+/**
+ * Validate data and return with error in case of fail.
+ */
+function validate(array $data, $rules)
+{
+    $validator = Validator::make($data, $rules);
+
+    if ($validator->fails()) {
+        return [
+            'valid' => false,
+            'messages' => $validator->messages()->toArray()
+        ];
+    }
+
+    return [
+        'valid' => true,
+        'messages' => ''
+    ];
+}
+
+/**
+ * Convert array to a stdClass.
+ */
+function makeObj(array $data) : object
+{
+    return (object)$data;
+}
+
+/**
+ * Takes in a path separated by forward slashes and returs the last part (file name).
+ */
+function fileNameFromPath1($path)
+{
+    $parts = explode('/', rtrim($path, '/'));
+    return end($parts);
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::prefix('v1')->group(function () {
+
+    Route::get('/', function() {
+        return view('welcome');
+    });
+
+    // Reports: list, show, store, update, delete
+    Route::apiResource('reports', ReportController::class)
+        ->missing('resourceNotFound')
+        ->shallow();
+
 });
